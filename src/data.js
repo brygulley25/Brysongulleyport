9c689f8f86a2bb876c34d1719935373821e14e7b
brygulley25 <brygulley25@gmail.com>
2023-05-20 13:40:56 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const projects = [
+  {
+    title: "Full stack app",
+    subtitle: "Movie review",
+    description:
+      "web application that allows users to search for and read reviews of movies, write their own reviews.",
+    link: "https://movie-review-8h71.onrender.com/",
+    image: "./movie review .gif",
+  },
+  {
+    title: "Acme Rockets project",
+    subtitle: "Responsive landing page made with Tailwind Css",
+    description:
+      "The Tailwind framework is a utility-first CSS framework that provides a set of pre-defined CSS classes that can be easily applied to HTML elements to create consistent and responsive layouts.",
+    link: "https://acme-rockets-axai.onrender.com/",
+    image: "./rockets.gif",
+  },
+  {
+    title: "Frontend Bootcamp project ",
+    subtitle: "React ",
+    description:
+      "this simple React project, are typically defined in JavaScript files and rendered to the DOM using JSX. This single page project makes use of props and state to manage the data displayed on the page",
+    link: "https://frontend-bootcamp3.onrender.com/",
+    image: "./frontend bootcamp.gif",
+  },
+  {
+    title: "Gulley Wine Project ",
+    subtitle: "UX/UI design to HTML & CSS",
+    description:
+      "From a Figma design to a live website, our project seamlessly transitioned from concept to reality. The Figma designs provided the blueprint for the website's layout, typography, and visual style. eCommerce project",
+    link: "https://statuesque-semifreddo-bcdae1.netlify.app/home",
+    image: "./FigmatoHTMl.gif",
+  },
+    {
+    title: "Divine Design  ",
+    subtitle: "Company site made with React",
+    description:
+      "Welcome to our company site! Here, you can easily get a free quote and learn everything you need to know about our brand. Our user-friendly interface allows you to access all the information you need in just a few clicks.",
+    link: "https://www.divinedesignllc.art/",
+    image: "./DivineDesign.gif",
+  },
+    {
+    title: "Midjourney/EditorX ",
+    subtitle: "Using Ai images",
+    description:
+      " The perfect blend of art and technology, brought to you by EditorX and Midjourney",
+    link: "https://brysongulley.editorx.io/aiart",
+    image: "./Midjourney.gif",
+  },
+  {
+    title: " React Native App ",
+    subtitle: "Using react native",
+    description:
+      " Job search app where you can press a job and apply ",
+    link: "https://expo.dev/@brygulley25/crashcourse?serviceType=classic&distribution=expo-go",
+    image: "./crashcourse.gif",
+  }
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "Figma",
+  "WordPress",
+  "MERN Stack",
+  "WebFlow"
+  
+  
+];
